a4f91002735a1c764c9ebb37b1bcc042a17edd69
cat-egory <xtheblackeyes@gmail.com>
2020-07-17 13:52:18 +0900

    add array

--- two-week-react/src/App.js
+++ two-week-react/src/App.js
@@ -5,14 +5,38 @@ import './App.css';
 import PhoneForm from './phone/PhoneForm';
 
 class App extends Component {
+  id = 2;
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: 'Go Moon Young',
+        phone: '010-0000-1111',
+      },
+      {
+        id: 1,
+        name: 'Gang TAE',
+        phone: '010-9999-8888',
+      },
+    ],
+  };
+
   handleCreate = (data) => {
     console.log('dd:', data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: (this.id += 1), ...data }),
+    });
   };
 
   render() {
+    const { information } = this.state;
+
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(information)}
       </div>
     );
   }
