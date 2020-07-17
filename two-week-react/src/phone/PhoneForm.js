0f86ab46c3e5c70b0cc0f88f805f8f832736939b
cat-egory <xtheblackeyes@gmail.com>
2020-07-17 13:10:16 +0900

    two input

--- two-week-react/src/phone/PhoneForm.js
+++ two-week-react/src/phone/PhoneForm.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class PhoneForm extends Component {
   state = {
     name: '',
+    phone: '',
   };
 
   handleChange = (e) => {
-    this.setState({ name: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
@@ -15,9 +16,17 @@ class PhoneForm extends Component {
         <input
           placeholder="name"
           value={this.state.name}
+          name="name"
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="phone"
+          value={this.state.phone}
+          name="phone"
           onChange={this.handleChange}
         />
         <h2>{this.state.name}</h2>
+        <h2>{this.state.phone}</h2>
       </form>
     );
   }
