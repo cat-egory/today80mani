4f7fb4a31aede9b71d0fdc02e119b817638a49df
cat-egory <xtheblackeyes@gmail.com>
2020-07-19 15:56:42 +0900

    add phoneInfoList

---
+++ two-week-react/src/phone/PhoneInfo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class PhoneInfo extends Component {
+  static defaultProps = {
+    info: {
+      name: 'myName',
+      phone: '1234',
+      id: 0,
+    },
+  };
+
+  render() {
+    const style = {
+      border: '1px solid border',
+      padding: '8px',
+      margin: '8px',
+    };
+
+    const { name, phone, id } = this.props.info;
+
+    return (
+      <div style={style}>
+        <div>
+          <b>{name}</b>
+        </div>
+        <div>{phone}</div>
+      </div>
+    );
+  }
+}
+
+export default PhoneInfo;
