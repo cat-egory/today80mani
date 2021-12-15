763e0769624c21f623fca1fe587f3a2cd8c23c9a
cat-egory <xtheblackeyes@gmail.com>
2021-12-16 07:51:50 +0900

    attact 프로젝트 커밋

---
+++ log4j-test/AttactLog4j/src/main/java/home/alex/attack/attactlog4j/AttactLog4jApplication.java
@@ -0,0 +1,13 @@
+package home.alex.attack.attactlog4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttactLog4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttactLog4jApplication.class, args);
+    }
+
+}
---
+++ log4j-test/AttactLog4j/src/test/java/home/alex/attack/attactlog4j/AttactLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package home.alex.attack.attactlog4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttactLog4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
